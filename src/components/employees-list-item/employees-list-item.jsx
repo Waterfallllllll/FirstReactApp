83e18874dfc998586903733f3e62b541f8c6bb0e
Turcan Ilya <ilieturcan424@gmail.com>
2024-12-31 15:46:19 +0200

    Add v0.1.0

--- src/components/employees-list-item/employees-list-item.jsx
+++ src/components/employees-list-item/employees-list-item.jsx
@@ -1,3 +1,5 @@
+import "./employees-list-item.css";
+
 const EmployeesListItem = () => {
     return (
         <li className="list-group-item d-flex justify-content-between">
@@ -7,6 +9,16 @@ const EmployeesListItem = () => {
                 className="list-group-item-input"
                 defaultValue="1000$"
             />
+            <div className="d-flex justify-content-center align-items-center">
+                <button className="btn-cookie btn-sm">
+                    <i className="fas fa-cookie"></i>
+                </button>
+
+                <button className="btn-trash btn-sm">
+                    <i className="fas fa-trash"></i>
+                </button>
+                <i className="fas fa-star"></i>
+            </div>
         </li>
     );
 };
